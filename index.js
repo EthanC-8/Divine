af14dc47f6c097665dc44cf6e09d9a99f54bc33d
Eshan Chaturvedi <33990363+EthanC-8@users.noreply.github.com>
2017-12-10 20:01:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ client.on('message', message => {
         case"music":
         message.channel.send(message.author+", Commands Sent to Dm")
         var embed = new Discord.RichEmbed()
-        .addField("-=[Music Commands]=-", "!"+musichelp)
+        .addField("-=[Music Commands]=-", "Music Feature is down for maintenance.")
         .setColor(0x00FDDF)
         message.author.send(embed);;
         break;
@@ -197,12 +197,192 @@ client.on('message', message => {
         if (server.dispatcher) server.dispatcher.end();
         message.reply('Audio skipped');
         console.log('Audio skipped').catch(console.error);
+const Discord = require("discord.js");
+const YTDL = require("ytdl-core");
+const YouTube = require("discord-youtube-api");
+const client = new Discord.Client();
+const token = "MzcwNTkwNTMxOTM3NjMyMjc2.DMpS-Q.fOeBqlmqCdqgQmAfgaaIhczLUhY";
+const prefix = "!!";
+
+function play(connection, message) {
+  var server = servers[message.guild.id];
+  server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: 'audioonly'}));
+  server.queue.shift();
+  server.dispatcher.on('end', function() {
+      if (server.queue[0]) play (connection, message);
+      else connection.disconnect();
+      console.log('Current Audio finished');
+  });
+}
+var fortune = [
+  "Yep",
+  "No rip...",
+  "Cant say..",
+  "Kinky try again xD",
+  "No idea sorz!",
+  " ¯\_(ツ)_/¯ "
+];
+var roast = [
+  ", I was about to give you a nasty look.. But, i see you already have one :>",
+  ", I am not saying that i hate you.. I'm just saying if you got hit by a bus, i would be driving that bus <3",
+  ", You remind me of my friend... Ug Lee",
+  ", You have two parts of brain, 'left' and 'right'. In the left side, there's nothing right. In the Right side, there's nothing left. :O",
+  ", Two Wrongs don't make a right, take your parents as an example..",
+  ", You sound reasonable. It must be time to up my medicaton!",
+  ", If laughter is the best medicine, your face must be curing the world..",
+  ", You are the proof that evolution CAN go in reverse",
+  ", i could have agreed with you but then, We both will be wrong",
+  " ¯\_(ツ)_/¯ "
+];
+var flipcoin = [
+  "Heads!.",
+  "Tales!."
+];
+var fun = [
+  "!!Hello - Say Hello.. \n !!Ping - Pong! \n !!Bored - Solution.. \n !!Fortune - Fortune Teller \n !!Flipcoin - Head or Tale.. \n !!Dab - Dab on Haters!.. \n !!Shoot - Shoot Someone \n !!Kill - Murderder Someone \n !!007 - James Bond!! \n !!Roast - Roast someone..",
+];
+var musichelp = [
+  "!!play [URl] - To add a song to queue.. \n !!skip - To skip the current song.. \n !!stop - To stop the music bot! \n  \n More music features to be added soon.. \n   "
+]
+var divineinfo = [
+  "!!Divine - Who is Divine?.. \n !!Creator - Creator of bot.. \n !!Version - Current Bot Version \n For any Question or help please Contact @Ethan8#1061 "
+]
+var musichelp = [
+  "!!Warn - Warn a User.. \n !!kick - Kick a user out of server..\n   "
+]
+var servers = {};
+var testc = "Pass";
+var emcolor = [
+  "0x00FDDF",
+  "0x05E463",
+  "0x05E4B5",
+  "0x0FDD00",
+  "0x5E00DD",
+  "0x3F2C5A",
+  "0xEEF813",
+  "0xF7A100"
+];
+
+
+
+//--------------------------------------
+client.on('ready', () => {
+  console.log('Online!');
+  client.user.setGame('With Eshan..')
+  client.user.setStatus("online")
+});
+
+client.on("guildMemberAdd", function(member) {
+  member.guild.channels.find("name", "general").send(member.toString() + "Welcome to the Commumnity!! Have a Great time here! <3");
+
+   member.addRole(member.guild.roles.find("name", "DM"));
+});
+
+//--------------------------------------
+
+//Commands.-------------------------------
+client.on('message', message => {
+  console.log(`(General) ${message.author.id}: ${message.content}`);
+ if (message.author.equals(client.user))return;
+ if (!message.content.startsWith(prefix)) return;
+      
+      var args = message.content.substring(prefix.length).split(" ");
+      console.log(`(Divine) ${message.author.id}: ${message.content}`);
+      
+      
+       switch (args[0].toLowerCase()) {
+        case"ping":
+        var embed = new Discord.RichEmbed()
+        .addField(new Date().getTime() - message.createdTimestamp + "ms. :alarm_clock: ", client.ping+"ms. :heartbeat:" )
+        .setColor(+emcolor[Math.floor(Math.random() * emcolor.length)])
+        message.channel.send(embed);
         break;
-    case 'stop':
-    var server = servers[message.guild.id];
-    
-    if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+        case"hello":
+        message.reply("Hey there, "+ message.author);
         break;
+        case"de":
+        var embed = new Discord.RichEmbed()
+        .addField("Devil :revolving_hearts: Angel", "     :angel::smiling_imp:" )
+        .setColor(+emcolor[Math.floor(Math.random() * emcolor.length)])
+        message.channel.send(embed);
+        break;
+        case"bored":
+        message.channel.send("Thats life, i cant do anything in that xD");
+        break;
+        case"divine":
+        message.channel.send("I am super duper Divine Bottt!! Type");
+        break;
+        case"fortune":
+        if (args[1]) message.channel.send(fortune[Math.floor(Math.random() * fortune.length)]);
+        else message.channel.send("The input text has too few parameters..");
+        break;
+        case"roast":
+        if (args[1]) message.channel.send(message.mentions.members.first()+roast[Math.floor(Math.random() * roast.length)]);
+        else message.channel.send("The input text has too few parameters..");
+        break;
+        case"flipcoin":
+        message.channel.send(flipcoin[Math.floor(Math.random() * flipcoin.length)]);
+        break;
+        case"help":
+        message.channel.send('-=[Divine Commands]=- \n !!fun - Fun Commands.. \n !!Music - Music Commands.. \n !!Info - About Divine Bot..\n !!MHelp - Moderators Commands ');
+        break;
+        case"music":
+        message.channel.send(message.author+", Commands Sent to Dm")
+        var embed = new Discord.RichEmbed()
+        .addField("-=[Music Commands]=-", "!"+musichelp)
+        .setColor(+emcolor[Math.floor(Math.random() * emcolor.length)])
+        message.author.send(embed);
+        break;
+        case"mhelp":
+        message.channel.send(message.author+", Commands Sent to Dm")
+        var embed = new Discord.RichEmbed()
+        .addField("-=[Moderaor Commands]=-", "!"+modhelp)
+        .setColor(+emcolor[Math.floor(Math.random() * emcolor.length)])
+        message.author.send(embed);;
+        break;
+        case"fun":
+        message.channel.send(message.author+", Commands Sent to Dm")
+        var embed = new Discord.RichEmbed()
+        .addField("-=[Fun Commands]=-", "!"+fun)
+        .setColor(+emcolor[Math.floor(Math.random() * emcolor.length)])
+        message.author.send(embed);;
+        break;
+        break;
+        case"info":
+        message.channel.send(message.author+", Commands Sent to Dm").catch(console.error)
+        var embed = new Discord.RichEmbed()
+        .addField("-=[About me]=-", "!"+divineinfo)
+        .setColor(+emcolor[Math.floor(Math.random() * emcolor.length)])
+        message.author.send(embed);;
+        break;
+        case"version":
+        var embed = new Discord.RichEmbed()
+        .addField("Version -", "4.15.2")
+        .setColor(+emcolor[Math.floor(Math.random() * emcolor.length)])
+        message.channel.send(embed);
+        break;
+        case"dab":
+        message.channel.sendMessage("<o/  \o/  \o>");
+        break;
+        case"shoot":
+        if (args[1]==message.mentions.members) message.channel.send("Pass");
+        if (args[1]) message.channel.send(message.author + " (╯°□°)--︻╦╤─ - - - " + message.mentions.members.first());
+        else message.channel.send("Whom to shoot? !!Shoot [Name]").catch(console.error);
+        break;
+        case"kill":
+        if (args[1]) message.channel.send(message.author + " has killed " + message.mentions.members.first()+"....*RIP*");
+        else message.channel.send("Who to kill? !!kill [Name]").catch(console.error);
+        break;
+        case"007":
+        message.channel.send("̿̿  ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з=( ͡° ͜ʖ ͡°)=ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿  ");         
+        break;
+        case"creator":
+        message.channel.send(" @Ethan8 Created this bot while breaking his head on walls :'D..");         
+        break;
+
+        //-----------=[Music Commands]=----------
+
+     //-------MUSIC COMMANDDS SAVED FOR MAINTANCE
 
        //-----------=[Music Commands]=----------
 
@@ -210,7 +390,7 @@ client.on('message', message => {
         case "warn":
         let modRole = message.guild.roles.find("name", "Mod");
         if(message.member.roles.has(modRole.id)){
-          if (args[1] , args[2]) message.channel.send(message.mentions.members.first()+", You have been warned for - " + args[2] );
+          if (args[1] , args[2]) message.channel.send(message.mentions.members.first()+", You have been warned for - " + args[2]);
           else message.channel.send("Format-  !!Warn [@user] [Reason]").catch(console.error);
         } 
         break;
@@ -229,7 +409,7 @@ client.on('message', message => {
           if (args[1]) {
           let messagecount = parseInt(args[1]);
           message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));                      
-          message.reply(" "+args[1]+" Messages have deleted..");
+          message.reply(+args[1]+" Messages have deleted..");
         }
       else {
         message.reply(" Please Enter number of messages to delete ");
@@ -255,5 +435,4 @@ client.on('msg', message => {
   msg.channel.sendMessage("Watch your Language please..");
   }
 });
-
 client.login(process.env.BOT_TOKEN);
