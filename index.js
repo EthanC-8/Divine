376ac364481fdcb827c304ab0ba6921b86756b1e
Eshan Chaturvedi <33990363+EthanC-8@users.noreply.github.com>
2018-02-04 01:12:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,11 +70,11 @@ var chathelp = [
 //--------------------------------------
 bot.on('ready', () => {
   console.log('Online!');
-//   bot.user.setGame('d!help | Being Useful')
-  bot.user.setActivity('d!help | Adventure Awaits', {
-    type: 'STREAMING',
-    url: 'https://twitch.tv/EshanEC',
-  });
+  bot.user.setGame('d!help | Adventure Awaits')
+//   bot.user.setActivity('d!help | Adventure Awaits', {
+//     type: 'STREAMING',
+//     url: 'https://twitch.tv/EshanEC',
+//   });
   bot.user.setStatus("online")
 });
 
